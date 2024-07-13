36e5ebe5715bf5589fe9fe59220fd285965f1d49
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2024-07-13 09:11:41 +0800

    show content in view Model

--- src/views/ViewMode.vue
+++ src/views/ViewMode.vue
@@ -1,1 +1,16 @@
-<template>This is View Mode</template>
+<script setup>
+import { computed } from "vue";
+import { nodes, links } from "@/stores/app.js";
+
+const node = computed(() => nodes.value[0]);
+const nodeLinks = computed(() => links.value.filter((c) => c.f === node.value));
+</script>
+<template>
+  <h2>This is View Mode</h2>
+  <div>
+    {{ node }}
+  </div>
+  <div v-for="c in nodeLinks">
+    {{ c }}
+  </div>
+</template>
